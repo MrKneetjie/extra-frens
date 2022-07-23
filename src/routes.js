cf45fbfb4e8776c18d18bdc1132c86b41d0203dd
David Knetemann <david.knetemann@studiohyperdrive.be>
2022-07-23 22:10:42 +0200

    first commit

---
+++ src/routes.js
@@ -0,0 +1,31 @@
+import { DEFAULT_PATHS } from 'config.js';
+import ComingSoon from 'views/ComingSoon';
+
+import HorizontalPage from 'views/Horizontal';
+
+const appRoot = DEFAULT_PATHS.APP.endsWith('/') ? DEFAULT_PATHS.APP.slice(1, DEFAULT_PATHS.APP.length) : DEFAULT_PATHS.APP;
+
+const routesAndMenuItems = {
+  mainMenuItems: [
+    {
+      path: DEFAULT_PATHS.APP,
+      exact: true,
+      redirect: true,
+      to: `${appRoot}/horizontal`,
+    },
+    // {
+    //   path: `${appRoot}/horizontal`,
+    //   component: HorizontalPage,
+    //   label: 'menu.horizontal',
+    //   icon: 'grid-2',
+    // },
+    {
+      path: `${appRoot}/soon`,
+      component: ComingSoon,
+      label: 'menu.coming-soon',
+      icon: 'calendar',
+    },
+  ],
+  sidebarItems: [],
+};
+export default routesAndMenuItems;
---
+++ src/views/ComingSoon.js
@@ -0,0 +1,179 @@
+
+import React, { useState, useEffect } from 'react';
+import { NavLink } from 'react-router-dom';
+import { Button, Col, Row } from 'react-bootstrap';
+import Countdown from 'react-countdown';
+import LayoutFullpage from 'layout/LayoutFullpage';
+import CsLineIcons from 'cs-line-icons/CsLineIcons';
+import HtmlHead from 'components/html-head/HtmlHead';
+import { MAILCHIMP } from 'constants.js';
+
+const mailchimp = require("@mailchimp/mailchimp_marketing");
+
+const simulateNetworkRequest = () => {
+  return new Promise((resolve) => setTimeout(resolve, 2000));
+}
+
+const ComingSoon = () => {
+  const [isLoading, setLoading] = useState(false);
+
+  const event = {
+    name: "Extra Frens MailList"
+  };
+  
+  const footerContactInfo = {
+    company: "ExtraFrens",
+    address1: "Extra",
+    address2: "Frens",
+    city: "Las Vegas",
+    state: "NV",
+    zip: "88901",
+    country: "US"
+  };
+  
+  const campaignDefaults = {
+    from_name: "Extra Frens",
+    from_email: "extrafrens@example.com",
+    subject: "Extra Frens MailList",
+    language: "EN_US"
+  };
+
+  const listId = "YOUR_LIST_ID";
+
+  const subscribingUser = {
+    firstName: "Prudence",
+    lastName: "McVankab",
+    email: "prudence.mcvankab@example.com"
+  };
+
+  useEffect(() => {
+    if (isLoading) {
+      mailchimp.setConfig({
+        apiKey: MAILCHIMP.ApiKey,
+        server: MAILCHIMP.ServerPrefix,
+      });
+
+      simulateNetworkRequest().then(() => {
+        setLoading(false);
+      });
+    }
+  }, [isLoading]);
+
+  const title = 'Coming Soon';
+  const description = 'Acorn will be available soon!';
+
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+  const date = 3 * day + 11 * hour + 20 * minute;
+
+
+  const submitEmail = async () => {
+    const response = await mailchimp.lists.createList({
+      name: event.name,
+      contact: footerContactInfo,
+      permission_reminder: "permission_reminder",
+      email_type_option: true,
+      campaign_defaults: campaignDefaults,
+    });
+
+    console.log(
+      `Successfully created an audience. The audience id is ${response.id}.`
+    );
+
+    // const response = await mailchimp.lists.addListMember(listId, {
+    //   email_address: subscribingUser.email,
+    //   status: "subscribed",
+    //   merge_fields: {
+    //     FNAME: subscribingUser.firstName,
+    //     LNAME: subscribingUser.lastName
+    //   }
+    // });
+  
+    // console.log(
+    //   `Successfully added contact as an audience member. The contact's id is ${
+    //     response.id
+    //   }.`
+    // );
+}
+
+  const renderer = ({ days, hours, minutes, seconds, completed }) => {
+    if (completed) {
+      // Render a completed state
+      return <p>Completed</p>;
+    }
+    // Render a countdown
+    return (
+      <div id="timer">
+        <Row className="g-2">
+          <Col xs="3">
+            <div className="display-5 text-primary mb-1">{days}</div>
+            <div>Days</div>
+          </Col>
+          <Col xs="3">
+            <div className="display-5 text-primary mb-1">{hours}</div>
+            <div>Hours</div>
+          </Col>
+          <Col xs="3">
+            <div className="display-5 text-primary mb-1">{minutes}</div>
+            <div>Minutes</div>
+          </Col>
+          <Col xs="3">
+            <div className="display-5 text-primary mb-1">{seconds}</div>
+            <div>Seconds</div>
+          </Col>
+        </Row>
+      </div>
+    );
+  };
+
+  // rightSide
+  const rightSide = (
+    <div className="sw-lg-80 min-h-100 bg-foreground d-flex justify-content-center align-items-center shadow-deep py-5 full-page-content-right-border full-page-content-border">
+      <div className="sw-lg-60 px-5">
+        <div className="sh-11">
+          <NavLink to="/">
+            <h1>ExtraFrens</h1>
+          </NavLink>
+        </div>
+        <div className="mb-3">
+          <h2 className="cta-1 mb-0 text-primary">ExtraFrens will be available soon!</h2>
+        </div>
+        <div className="mb-5 sh-9">
+          <Countdown date={Date.now() + date} intervalDelay={0} precision={3} renderer={renderer} />
+        </div>
+        <div className="mb-3">
+          <p className="h6 lh-1-5">
+            To receive our newsletter please complete the form below. We take your privacy seriously and we will not share your information with others.
+          </p>
+        </div>
+
+        <div>
+          <form>
+            <div className="mb-3 filled">
+              <CsLineIcons icon="email" />
+              <input className="form-control" id="password" type="password" placeholder="Email" />
+            </div>
+            <Button className="btn-icon btn-icon-end" onClick={!isLoading ? submitEmail : null}>
+              <span>Submit</span>
+              <CsLineIcons icon="chevron-right" />
+            </Button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+
+  // leftSide
+  const leftSide = <></>;
+
+  return (
+    <>
+      <HtmlHead title={title} description={description} />
+      <LayoutFullpage right={rightSide} left={leftSide} />
+    </>
+  );
+};
+
+export default ComingSoon;
